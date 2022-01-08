1b81bb5e542aeed9140b833e735d640257d4d874
Tom Strat Laptop <tomstratfic@hotmail.com>
2022-01-08 22:22:09 +0000

    added server

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,32 @@
 import "https://deno.land/x/dotenv/load.ts";
+// import { serveTls } from "https://deno.land/std/http/mod.ts";
 
 import {opine, OpineRequest, OpineResponse} from "./deps.ts"
 
 const app = opine()
 
+const redirect = `https://login.salesforce.com/services/oauth2/authorize?client_id=${Deno.env.get("CONSUMER_KEY")}&redirect_uri=http://localhost:3000/oauth2/callback&response_type=code`
+
+// const key = "../keys/key.pem"
+// const cert = "../keys/cert.pem"
+
 app.get("/", (req: OpineRequest, res: OpineResponse) => {
-  res.send("Hello")
+  res.send(`<button onclick="window.location.replace('${redirect}')">Login</button>`)
 })
+
+app.get("/oauth2/callback", (req: OpineRequest, res: OpineResponse) => {
+  res.send(req.params.code)
+})
+
+// function reqHandler(req: Request) {
+//   return new Response("Secure Hello world");
+// }
+
+// serveTls(reqHandler, {port: 3000, cert, key})
+// console.log("Server has started on https://localhost:3000")
 
 app.listen(
   3000,
   () => console.log("Server has started on http://localhost:3000")
 )
-// const SFURL = "https://spotlightdevelopment2-dev-ed.my.salesforce.com/services/oauth2/token"
 
-// const token = await fetch(SFURL, {
-//   method: "POST",
-//   body: `grant_type=client_credentials&client_id=${Deno.env.get("CONSUMER_KEY")}&client_secret=${Deno.env.get("CONSUMER_SECRET")}`,
-//   headers: {
-//     "Content-Type": "application/x-www-form-urlencoded"
-//   }
-// })
