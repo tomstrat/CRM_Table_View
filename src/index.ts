d5235db08eca639534d4cb5c12935a090f0c9ef7
Tom Strat <tomstrat@pop-os.localdomain>
2022-01-10 00:01:39 +0000

    add root route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import express from "express"
+import express, {Request, Response} from "express"
 import authGetRouter from "./routes/auth/get"
 
 const app = express()
 
 app.use("/oauth2", authGetRouter)
+
+app.get("/", (req: Request, res: Response) => {
+  res.redirect("/oauth2/login")
+})
 
 app.listen(
   3000,
