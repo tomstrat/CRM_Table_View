ae920b19673cdefc5b84eee1d3307eb51aadd3bd
Tom Strat <tomstrat@pop-os.localdomain>
2022-01-15 23:47:26 +0000

    update login css

--- src/views/layout.ts
+++ src/views/layout.ts
@@ -6,7 +6,7 @@ export default function layout(template: string): string {
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <link rel="stylesheet" type="text/css" href="css/style.css" />
+          <link rel="stylesheet" type="text/css" href="/css/style.css" />
           <title>CRM Table Viewer</title>
       </head>
       <body>
