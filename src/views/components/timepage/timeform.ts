384d0694a6677b264e1651c41caf96767f7d4cad
Whiskerz1337 <luke.bailey2014@gmail.com>
2022-01-21 14:22:05 +1100

    css

--- src/views/components/timepage/timeform.ts
+++ src/views/components/timepage/timeform.ts
@@ -14,22 +14,20 @@ export default function timeform(): string {
             </div>
         </div>
         <div class="break-container">  
-            <form class="breakbutton-container">
-                <p1>Did you take a break?</p1>
-                <div class="switch-field">
-                    <input type="radio" id="radio-one" name="switch-one" value="yes" checked/>
-                    <label for="radio-one">Yes</label>
-                    <input type="radio" id="radio-two" name="switch-one" value="no" />
-                    <label for="radio-two">No</label>
-                </div>
-            </form>
+            <label>Break?</label>
+            <div class="switch-field">
+                <input type="radio" id="radio-one" name="switch-one" value="yes" checked/>
+                <label for="radio-one">Yes</label>
+                <input type="radio" id="radio-two" name="switch-one" value="no" />
+                <label for="radio-two">No</label>
+            </div>
         </div>
         <div class="break-time-container">
         <label for="break-start">Break start time:</label>
         <input type="time" class="timedrop" name="break-start"
         min="05:00" max="23:00" required>
     
-        <label for="break-end">Break end time:</label>
+        <label for="break-end">until</label>
         <input type="time" class="timedrop" name="break-end"
         min="05:00" max="23:00" required>
         </div>
--- src/views/ttmavailability.ts
+++ src/views/ttmavailability.ts
@@ -2,6 +2,14 @@ import layout from "./layout"
 
 export default function ttmavailability(): string {
     return layout(`
-      
+    <form class="breakbutton-container">
+    <p1>Did you take a break?</p1>
+    <div class="switch-field">
+        <input type="radio" id="radio-one" name="switch-one" value="yes" checked/>
+        <label for="radio-one">Yes</label>
+        <input type="radio" id="radio-two" name="switch-one" value="no" />
+        <label for="radio-two">No</label>
+    </div>
+</form>
     `)
 }
