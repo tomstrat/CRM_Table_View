3bece0760756f560e2d9f19c5acaac2c8b04a7aa
Whiskerz1337 <luke.bailey2014@gmail.com>
2022-01-22 10:59:13 +1100

    fdsf

--- src/views/ttmhours.ts
+++ src/views/ttmhours.ts
@@ -1,7 +1,15 @@
+import timeform from "./components/timepage/timeform"
 import layout from "./layout"
 
-export default function ttmhours(): string {
+export default function timePage(): string {
     return layout(`
-      
+    <div class="day-container">
+        <label for="day">Select day:</label>
+        <input type="date" id="start" name="day"
+            value="2018-07-22"
+            min="2018-01-01" max="2018-12-31">
+    </div>
+    ${timeform()}
+                
     `)
 }
