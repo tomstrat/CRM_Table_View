b7385be01154135c7fd43284f4434bcf27884389
Whiskerz1337 <luke.bailey2014@gmail.com>
2022-01-24 11:17:31 +1100

    css

--- src/views/ttmviews/timesheets/ttmoverview.ts
+++ src/views/ttmviews/timesheets/ttmoverview.ts
@@ -2,8 +2,7 @@ import layout from "../../layout"
 import timenav from "../../components/layout/timenav"
 
 export default function ttmoverview(): string {
-    return layout(`
-    ${timenav()}
+    return layout(`${timenav()}
     
     `)
 }
