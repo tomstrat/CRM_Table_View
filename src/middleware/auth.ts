c68141b951a4881fe2b3c93d6923a1cfad5e01c6
Tom Strat <tomstrat@pop-os.localdomain>
2022-01-26 00:16:51 +0000

    fix bugs

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,8 +4,7 @@ import { Unauthorised } from "../models/error"
 
 export default function requireAuth(role?: Role): RequestHandler {
   return (req: Request, res: Response, next: NextFunction) => {
-    console.log("ROLE IS", role)
-    if (!role === undefined || req.session && req.session.role === role) {
+    if (role === undefined || req.session && req.session.role === role) {
       next()
     } else {
       throw new Unauthorised("You dont have the correct authorisation")
