84a9ca2580e473986b4953262e8f6a4348dd13ac
Tom Strat <tomstrat@pop-os.localdomain>
2022-01-28 23:08:51 +0000

    Add frontend js functions

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -3,35 +3,35 @@ let newUserButtons = document.querySelectorAll(".new-user-button")
 let rosterButton = document.querySelectorAll(".roster-toggle")
 let dayButtons = document.querySelectorAll(".availability-button")
 
-var Myelement = document.forms['new-user-form']['invis-mon'];
+var Myelement = document.forms["new-user-form"]["invis-mon"]
 
 newUserButtons.forEach(element => {
   element.addEventListener("click", sidebarSwitch)
- });
+ })
 
 controlsButtons.forEach(element => {
  element.addEventListener("click", buttonHighlight)
-});
+})
 
 rosterButton.forEach(element => {
   element.addEventListener("click", rosterToggle)
-});
+})
 
 dayButtons.forEach(element => {
   element.addEventListener("click", dayToggle)
-});
+})
 
 function sidebarSwitch() {
-  if (document.querySelector('#default-sidebar').classList.contains('visible-sidebar')) {
-    document.querySelector('#default-sidebar').classList.remove('visible-sidebar')
-    document.querySelector('#default-sidebar').classList.add('invisible-sidebar')
-    document.querySelector('#secondary-sidebar').classList.add('visible-sidebar')
-    document.querySelector('#secondary-sidebar').classList.remove('invisible-sidebar')
+  if (document.querySelector("#default-sidebar").classList.contains("visible-sidebar")) {
+    document.querySelector("#default-sidebar").classList.remove("visible-sidebar")
+    document.querySelector("#default-sidebar").classList.add("invisible-sidebar")
+    document.querySelector("#secondary-sidebar").classList.add("visible-sidebar")
+    document.querySelector("#secondary-sidebar").classList.remove("invisible-sidebar")
   } else {
-    document.querySelector('#default-sidebar').classList.add('visible-sidebar')
-    document.querySelector('#default-sidebar').classList.remove('invisible-sidebar')
-    document.querySelector('#secondary-sidebar').classList.remove('visible-sidebar')
-    document.querySelector('#secondary-sidebar').classList.add('invisible-sidebar')
+    document.querySelector("#default-sidebar").classList.add("visible-sidebar")
+    document.querySelector("#default-sidebar").classList.remove("invisible-sidebar")
+    document.querySelector("#secondary-sidebar").classList.remove("visible-sidebar")
+    document.querySelector("#secondary-sidebar").classList.add("invisible-sidebar")
   }
 }
 
@@ -50,36 +50,36 @@ function rosterToggle(e) {
   if (e.target.classList.contains("roster-toggle-off")) {
     e.target.classList.add("roster-toggle-on")
     e.target.classList.remove("roster-toggle-off")
-    document.querySelector('#roster-container').classList.add('avail-display-on')
-    document.querySelector('#roster-container').classList.remove('avail-display-off')
+    document.querySelector("#roster-container").classList.add("avail-display-on")
+    document.querySelector("#roster-container").classList.remove("avail-display-off")
   } else {
     e.target.classList.add("roster-toggle-off")
     e.target.classList.remove("roster-toggle-on")
-    document.querySelector('#roster-container').classList.add('avail-display-off')
-    document.querySelector('#roster-container').classList.remove('avail-display-on')
+    document.querySelector("#roster-container").classList.add("avail-display-off")
+    document.querySelector("#roster-container").classList.remove("avail-display-on")
 }
 }
 
 function dayToggle (e) {
-  let idswitched = e.target.id.replace('avail', 'invis')
-  let otherElement = document.forms['new-user-form'][idswitched]
+  let idswitched = e.target.id.replace("avail", "invis")
+  let otherElement = document.forms["new-user-form"][idswitched]
   
 
   if (e.target.classList.contains("avail-none")) {
     e.target.classList.add("avail-green")
     e.target.classList.remove("avail-none")
-    otherElement.setAttribute('value', 'Working')
+    otherElement.setAttribute("value", "Working")
   } else if (e.target.classList.contains("avail-green")){
       e.target.classList.add("avail-yellow")
       e.target.classList.remove("avail-green")
-      otherElement.setAttribute('value', '?')
+      otherElement.setAttribute("value", "?")
   } else if (e.target.classList.contains("avail-yellow")){
       e.target.classList.add("avail-red")
       e.target.classList.remove("avail-yellow")
-      otherElement.setAttribute('value', 'Not working')
+      otherElement.setAttribute("value", "Not working")
   } else if (e.target.classList.contains("avail-red")){
       e.target.classList.add("avail-none")
       e.target.classList.remove("avail-red")
-      otherElement.setAttribute('value', '')
+      otherElement.setAttribute("value", "")
 }
 }
