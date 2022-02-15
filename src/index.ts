d2e113a7c9d8c2b2f952ba6284615e833df77983
Tom Strat <tomstrat@pop-os.localdomain>
2022-02-15 23:35:39 +0000

    update port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import inject from "./registry"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3001
 
 const app = inject()
   .then(app => {
     app.listen(
-      3001,
+      PORT,
       () => console.log("Server has started on http://localhost:3001")
     )
   })
